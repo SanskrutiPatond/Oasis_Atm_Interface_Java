70aba013a84f79c4b448d8dba735a76f70093961
SanskrutiPatond <125470326+SanskrutiPatond@users.noreply.github.com>
2023-02-16 15:25:28 +0530

    Add files via upload

---
+++ AtmAnother.java
@@ -0,0 +1,9 @@
+package pack.Atm;
+
+public interface AtmAnother
+{
+    public void viewBalance();
+    public void withdrawAmount(double withrawAmount);
+    public void  depositAmount(double depositAmount);
+    public void viewtransaction_history();
+}
